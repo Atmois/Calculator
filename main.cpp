193cc50a54fed6a382ff6b4b3c251cd36451ae08
Atmois <130537361+Atmois@users.noreply.github.com>
2023-11-01 22:50:45 +0100

    rewrote the code better

--- main.cpp
+++ main.cpp
@@ -3,30 +3,34 @@
 
 using str = std::string;
 
-double add(double num1, double num2)
+double arithmetic(double num1, double num2, str calcOperator)
 {
-    return num1 + num2;
-}
-
-double subtract(double num1, double num2)
-{
-    return num1 - num2;
-}
-
-double multiply(double num1, double num2)
-{
-    return num1 * num2;
-}
-
-double divide(double num1, double num2)
-{
-    if (num2 == 0)
+    if (calcOperator == "+")
     {
-        return 0;
+        return num1 + num2;
+    }
+    else if (calcOperator == "-")
+    {
+        return num1 - num2;
+    }
+    else if (calcOperator == "*")
+    {
+        return num1 * num2;
+    }
+    else if (calcOperator == "/")
+    {
+        if (num2 == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return num1 / num2;
+        }
     }
     else
     {
-        return num1 / num2;
+        return std::numeric_limits<double>::quiet_NaN();
     }
 }
 
@@ -46,38 +50,22 @@ int main()
     std::cin >> num1;
     std::cout << "Add('+'), Subtract('-'), Divde('/'), Multiply('*') or Powers('^'): ";
     std::cin >> calcOperator;
-    if (calcOperator != "^")
+
+    if (calcOperator == "+" || calcOperator == "-" || calcOperator == "/" || calcOperator == "*")
     {
         std::cout << "Number 2: ";
         std::cin >> num2;
-    }
-
-    if (calcOperator == "+")
-    {
-        ans = add(num1, num2);
-        std::cout << num1 << "+" << num2 << "=" << ans;
-    }
-    else if (calcOperator == "-")
-    {
-        ans = subtract(num1, num2);
-        std::cout << num1 << "-" << num2 << "=" << ans;
-    }
-    else if (calcOperator == "*")
-    {
-        ans = multiply(num1, num2);
-        std::cout << num1 << "*" << num2 << "=" << ans;
-    }
-    else if (calcOperator == "/")
-    {
-        ans = divide(num1, num2);
-        if(ans == 0) {
+        ans = arithmetic(num1, num2, calcOperator);
+        if (calcOperator == "/" && ans == 0)
+        {
             std::cout << "Error: You cannot divide by 0";
         }
-        else {
-            std::cout << num1 << "/" << num2 << "=" << ans; 
+        else
+        {
+            std::cout << num1 << calcOperator << num2 << "=" << ans;
         }
-        
     }
+
     else if (calcOperator == "^")
     {
         std::cout << "What power would you like: ";
@@ -85,9 +73,12 @@ int main()
         ans = power(num1, num2);
         std::cout << num1 << "^" << num2 << "=" << ans;
     }
+
     else
     {
         std::cout << "Error: Invalid Operator\n";
     }
+
+    
     return 0;
 }
