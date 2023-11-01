cee493f2c4a7ddb88358a36fa5c658141761b433
Atmois <130537361+Atmois@users.noreply.github.com>
2023-11-01 23:01:13 +0100

    added error handling

--- main.cpp
+++ main.cpp
@@ -48,37 +48,56 @@ int main()
 
     std::cout << "Number 1: ";
     std::cin >> num1;
-    std::cout << "Add('+'), Subtract('-'), Divde('/'), Multiply('*') or Powers('^'): ";
-    std::cin >> calcOperator;
-
-    if (calcOperator == "+" || calcOperator == "-" || calcOperator == "/" || calcOperator == "*")
+    if (std::isnan(num1))
     {
-        std::cout << "Number 2: ";
-        std::cin >> num2;
-        ans = arithmetic(num1, num2, calcOperator);
-        if (calcOperator == "/" && ans == 0)
-        {
-            std::cout << "Error: You cannot divide by 0";
-        }
-        else
-        {
-            std::cout << num1 << calcOperator << num2 << "=" << ans;
-        }
+        std::cout << "Error: Not a number";
     }
-
-    else if (calcOperator == "^")
-    {
-        std::cout << "What power would you like: ";
-        std::cin >> num2;
-        ans = power(num1, num2);
-        std::cout << num1 << "^" << num2 << "=" << ans;
-    }
-
     else
     {
-        std::cout << "Error: Invalid Operator\n";
-    }
+        std::cout << "Add('+'), Subtract('-'), Divde('/'), Multiply('*') or Powers('^'): ";
+        std::cin >> calcOperator;
 
-    
+        if (calcOperator == "+" || calcOperator == "-" || calcOperator == "/" || calcOperator == "*")
+        {
+            std::cout << "Number 2: ";
+            std::cin >> num2;
+            if (std::isnan(num2))
+            {
+                std::cout << "Error: Not a number";
+            }
+            else
+            {
+                ans = arithmetic(num1, num2, calcOperator);
+                if (calcOperator == "/" && ans == 0)
+                {
+                    std::cout << "Error: You cannot divide by 0";
+                }
+                else
+                {
+                    std::cout << num1 << calcOperator << num2 << "=" << ans;
+                }
+            }
+        }
+
+        else if (calcOperator == "^")
+        {
+            std::cout << "What power would you like: ";
+            std::cin >> num2;
+            if (std::isnan(num2))
+            {
+                std::cout << "Error: Not a number";
+            }
+            else
+            {
+                ans = power(num1, num2);
+                std::cout << num1 << "^" << num2 << "=" << ans;
+            }
+        }
+
+        else
+        {
+            std::cout << "Error: Invalid Operator\n";
+        }
+    }
     return 0;
 }
