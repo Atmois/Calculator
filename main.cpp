ee9ed25c41f5cccd3d2b57ce768d533e89834d71
Atmois <130537361+Atmois@users.noreply.github.com>
2023-11-02 09:13:06 +0100

    add better error handling

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 
 using str = std::string;
 
+double num1;
+double num2;
+double ans;
+str calcOperator;
+str calcProcess;
+
 double arithmetic(double num1, double num2, str calcOperator)
 {
     if (calcOperator == "+")
@@ -39,65 +45,87 @@ double power(double num1, double num2)
     return pow(num1, num2);
 }
 
-int main()
+str calculation(str calcOperator)
 {
-    double num1;
-    double num2;
-    double ans;
-    str calcOperator;
-
-    std::cout << "Number 1: ";
-    std::cin >> num1;
-    if (std::isnan(num1))
+    if (calcOperator == "+" || calcOperator == "-" || calcOperator == "/" || calcOperator == "*")
     {
-        std::cout << "Error: Not a number";
+        std::cout << "Number 1: ";
+        while (!(std::cin >> num1))
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Error: You entered a non numerical character for Number 1\n";
+            std::cout << "Number 1: ";
+        }
+        std::cout << "Number 2: ";
+        while (!(std::cin >> num2))
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Error: You entered a non numerical character for Number 2\n";
+            std::cout << "Number 2: ";
+        }
+
+        ans = arithmetic(num1, num2, calcOperator);
+        if (calcOperator == "/" && ans == 0)
+        {
+            std::cout << "Error: You cannot divide by 0";
+            return "invDivison";
+        }
+        else
+        {
+            std::cout << num1 << calcOperator << num2 << "=" << ans;
+            return "success";
+        }
+    }
+    else if (calcOperator == "^")
+    {
+        std::cout << "Number 1: ";
+        while (!(std::cin >> num1))
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Error: You entered a non numerical character for Number 1\n";
+            std::cout << "Number 1: ";
+        }
+        std::cout << "Power: ";
+        while (!(std::cin >> num2))
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Error: You entered a non numerical character for Power\n";
+            std::cout << "Power: ";
+        }
+            ans = power(num1, num2);
+            std::cout << num1 << "^" << num2 << "=" << ans;
+            return "success";
     }
     else
     {
-        std::cout << "Add('+'), Subtract('-'), Divde('/'), Multiply('*') or Powers('^'): ";
-        std::cin >> calcOperator;
+        std::cout << "Error: Invalid Operator\n";
+        return "invOp";
+    }
+    return "failed";
+}
 
-        if (calcOperator == "+" || calcOperator == "-" || calcOperator == "/" || calcOperator == "*")
-        {
-            std::cout << "Number 2: ";
-            std::cin >> num2;
-            if (std::isnan(num2))
-            {
-                std::cout << "Error: Not a number";
-            }
-            else
-            {
-                ans = arithmetic(num1, num2, calcOperator);
-                if (calcOperator == "/" && ans == 0)
-                {
-                    std::cout << "Error: You cannot divide by 0";
-                }
-                else
-                {
-                    std::cout << num1 << calcOperator << num2 << "=" << ans;
-                }
-            }
-        }
+str getOperator()
+{
+    std::cout << "Add('+'), Subtract('-'), Divde('/'), Multiply('*') or Powers('^'): ";
+    std::cin >> calcOperator;
+    return calcOperator;
+}
 
-        else if (calcOperator == "^")
-        {
-            std::cout << "What power would you like: ";
-            std::cin >> num2;
-            if (std::isnan(num2))
-            {
-                std::cout << "Error: Not a number";
-            }
-            else
-            {
-                ans = power(num1, num2);
-                std::cout << num1 << "^" << num2 << "=" << ans;
-            }
-        }
-
-        else
-        {
-            std::cout << "Error: Invalid Operator\n";
-        }
+int main()
+{
+    calcOperator = getOperator();
+    calcProcess = calculation(calcOperator);
+    if (calcProcess == "failed")
+    {
+        std::cout << "FAILED";
+    }
+    else if (calcProcess == "invOp" || calcProcess == "invDivision")
+    {
+        main();
     }
     return 0;
 }
