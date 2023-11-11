4c5cd4414f7fcc2167216150b0c8eb9e60d699e6
Atmois <atmois@outlook.com>
2023-11-11 15:46:02 +0000

    remove str type alias

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <cmath>
 #include <iostream>
-using str = std::string;
 
 /* TO DO
 - Add trig stuff
@@ -9,185 +8,147 @@ using str = std::string;
 - Maybe make a GUI?
 */
 
-double inputNum1(int num1)
-{
-    std::cout << "Number 1: ";
-    while (!(std::cin >> num1))
-    {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Error: You entered a non numerical character for Number 1" << "\n" << "Number 1: ";
-    }
-    return num1;
+double inputNum1(int num1) {
+  std::cout << "Number 1: ";
+  while (!(std::cin >> num1)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Error: You entered a non numerical character for Number 1"
+              << "\n"
+              << "Number 1: ";
+  }
+  return num1;
 }
-double inputNum2(int num2)
-{
-    std::cout << "Number 2: ";
-    while (!(std::cin >> num2))
-    {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Error: You entered a non numerical character for Number 2" << "\n" << "Number 2: ";
-    }
-    return num2;
-}
-
-double arithmetic(double num1, double num2, str calcOperator)
-{
-    if (calcOperator == "+")
-    {
-        return num1 + num2;
-    }
-    else if (calcOperator == "-")
-    {
-        return num1 - num2;
-    }
-    else if (calcOperator == "*")
-    {
-        return num1 * num2;
-    }
-    else if (calcOperator == "/")
-    {
-        return num1 / num2;
-    }
-    return std::numeric_limits<double>::quiet_NaN();
+double inputNum2(int num2) {
+  std::cout << "Number 2: ";
+  while (!(std::cin >> num2)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Error: You entered a non numerical character for Number 2"
+              << "\n"
+              << "Number 2: ";
+  }
+  return num2;
 }
 
-double power(double num1, double num2)
-{
+double arithmetic(double num1, double num2, std::string calcOperator) {
+  if (calcOperator == "+") {
+    return num1 + num2;
+  } else if (calcOperator == "-") {
+    return num1 - num2;
+  } else if (calcOperator == "*") {
+    return num1 * num2;
+  } else if (calcOperator == "/") {
+    return num1 / num2;
+  }
+  return std::numeric_limits<double>::quiet_NaN();
+}
+
+double power(double num1, double num2) {
+  num1 = inputNum1(num1);
+  std::cout << "Power: ";
+  while (!(std::cin >> num2)) {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Error: You entered a non numerical character for Power"
+              << "\n"
+              << "Power: ";
+  }
+  return pow(num1, num2);
+}
+
+std::string trigCalc(std::string calcOperator, double num1, double num2) {}
+
+std::string arithmeticCalc(std::string calcOperator, double num1, double num2) {
+  double ans;
+  if (calcOperator == "+" || calcOperator == "-" || calcOperator == "/" || calcOperator == "*") {
     num1 = inputNum1(num1);
-    std::cout << "Power: ";
-    while (!(std::cin >> num2))
-    {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Error: You entered a non numerical character for Power" << "\n" << "Power: ";
+    num2 = inputNum2(num2);
+    while (calcOperator == "/" && num2 == 0) {
+      std::cout << "Error: You cannot divide by 0\n";
+      num2 = inputNum2(num2);
     }
-    return pow(num1, num2);
+    ans = arithmetic(num1, num2, calcOperator);
+    std::cout << num1 << calcOperator << num2 << "=" << ans;
+    return "success";
+  } else if (calcOperator == "^") {
+    ans = power(num1, num2);
+    std::cout << num1 << "^" << num2 << "=" << ans;
+    return "success";
+  } else {
+    std::cout << "Error: Invalid Operator\n";
+    return "invOp";
+  }
+  return "failed";
 }
 
-str trigCalc(str calcOperator, double num1, double num2)
-{
+std::string getOperator(char calcMode, std::string calcOperator) {
+  if (calcMode == 'a') {
+    std::cout << "Add('+'), Subtract('-'), Divde('/'), Multiply('*') or Powers('^'): ";
+    std::cin >> calcOperator;
+  } else if (calcMode == 't') {
+    std::cout << "Sine(S), Cosine(C), Tan(T), Sine Inverse(SI), Cosine Inverse(CI), Tan Inverse(TI), Cosine Rule(CR) or Sine Rule(SR): ";
+    std::cin >> calcOperator;
+    for (int i = 0; i < calcOperator.length(); i++) {
+      calcOperator[i] = (char)std::tolower(calcOperator[i]);
+    }
+  }
+  return calcOperator;
 }
 
-str arithmeticCalc(str calcOperator, double num1, double num2)
-{
-    double ans;
-    if (calcOperator == "+" || calcOperator == "-" || calcOperator == "/" || calcOperator == "*")
-    {
-        num1 = inputNum1(num1);
-        num2 = inputNum2(num2);
-        while (calcOperator == "/" && num2 == 0)
-        {
-            std::cout << "Error: You cannot divide by 0\n";
-            num2 = inputNum2(num2);
-        }
-        ans = arithmetic(num1, num2, calcOperator);
-        std::cout << num1 << calcOperator << num2 << "=" << ans;
-        return "success";
+char getMode(char calcMode) {
+  bool validMode = false;
+  while (validMode == false) {
+    std::cout << "Would you like to do Arithmetic('A') or Trig('T'): ";
+    std::cin >> calcMode;
+    calcMode = (char)std::tolower(calcMode);
+    if (calcMode == 'a' || calcMode == 't') {
+      validMode = true;
+    } else {
+      std::cout << "Error: Invalid Mode\n";
     }
-    else if (calcOperator == "^")
-    {
-        ans = power(num1, num2);
-        std::cout << num1 << "^" << num2 << "=" << ans;
-        return "success";
-    }
-    else
-    {
-        std::cout << "Error: Invalid Operator\n";
-        return "invOp";
-    }
-    return "failed";
+  }
+
+  return calcMode;
 }
 
-str getOperator(char calcMode, str calcOperator)
-{
-    if (calcMode == 'a')
-    {
-        std::cout << "Add('+'), Subtract('-'), Divde('/'), Multiply('*') or Powers('^'): ";
-        std::cin >> calcOperator;
-    }
-    else if (calcMode == 't')
-    {   
-        std::cout << "Sine(S), Cosine(C), Tan(T), Sine Inverse(SI), Cosine Inverse(CI), Tan Inverse(TI), Cosine Rule(CR) or Sine Rule(SR): ";
-        std::cin >> calcOperator;
-        for (int i = 0; i < calcOperator.length(); i++)
-        {
-            calcOperator[i] = (char)std::tolower(calcOperator[i]);
-        }
-    }
-    return calcOperator;
+char continueCheck(std::string calcProcess, char continueProgram) {
+
+  std::cout << "\nWould you like to use the calculator again (Y/N): ";
+  std::cin >> continueProgram;
+  continueProgram = (char)std::tolower(continueProgram);
+
+  return continueProgram;
 }
 
-char getMode(char calcMode)
-{
-    bool validMode = false;
-    while (validMode == false)
-    {
-        std::cout << "Would you like to do Arithmetic('A') or Trig('T'): ";
-        std::cin >> calcMode;
-        calcMode = (char)std::tolower(calcMode);
-        if (calcMode == 'a' || calcMode == 't')
-        {
-            validMode = true;
-        }
-        else
-        {
-            std::cout << "Error: Invalid Mode\n";
-        }
+int main() {
+  char continueProgram = 'y';
+  while (continueProgram == 'y') {
+    double num1;
+    double num2;
+    std::string calcOperator;
+    std::string calcProcess;
+    char calcMode;
+
+    calcMode = getMode(calcMode);
+    calcOperator = getOperator(calcMode, calcOperator);
+    if (calcMode == 'a') {
+      calcProcess = arithmeticCalc(calcOperator, num1, num2);
+    } else if (calcMode == 't') {
+      calcProcess = trigCalc(calcOperator, num1, num2);
     }
 
-    return calcMode;
-}
+    // -------------------------------------------- //
 
-char continueCheck(str calcProcess, char continueProgram)
-{
-
-    std::cout << "\nWould you like to use the calculator again (Y/N): ";
-    std::cin >> continueProgram;
-    continueProgram = (char)std::tolower(continueProgram);
-
-    return continueProgram;
-}
-
-int main()
-{
-    char continueProgram = 'y';
-    while (continueProgram == 'y')
-    {
-        double num1;
-        double num2;
-        str calcOperator;
-        str calcProcess;
-        char calcMode;
-
-        calcMode = getMode(calcMode);
-        calcOperator = getOperator(calcMode, calcOperator);
-        if (calcMode == 'a')
-        {
-            calcProcess = arithmeticCalc(calcOperator, num1, num2);
-        }
-        else if (calcMode == 't')
-        {
-            calcProcess = trigCalc(calcOperator, num1, num2);
-        }
-
-        // -------------------------------------------- //
-
-        if (calcProcess == "failed")
-        {
-            std::cout << "Error";
-            continue;
-        }
-        else if (calcProcess == "invOp")
-        {
-            continue;
-        }
-        else if (calcProcess == "success")
-        {
-            continueProgram = continueCheck(calcProcess, continueProgram);
-        }
+    if (calcProcess == "failed") {
+      std::cout << "Error";
+      continue;
+    } else if (calcProcess == "invOp") {
+      continue;
+    } else if (calcProcess == "success") {
+      continueProgram = continueCheck(calcProcess, continueProgram);
     }
+  }
 
-    return 0;
+  return 0;
 }
